2e6892b0fe8f9558100c5883500938084cbc5a78
hyuk <tig05205@naver.com>
2024-05-07 15:58:54 +0900

    join

--- src/main/java/lectureedu/user/UserDAO.java
+++ src/main/java/lectureedu/user/UserDAO.java
@@ -89,45 +89,29 @@ public class UserDAO {
 
     public boolean getUserEmailChecked(String userID){
         String SQL = "SELECT userEmailChecked FROM USER WHERE userID = ?";
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try{
-            conn = DatabaseUtil.getConnection();
-            pstmt = conn.prepareStatement(SQL);
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
             pstmt.setString(1, userID);
             rs = pstmt.executeQuery();
-            if (rs.next()) {
-                return rs.getBoolean(1);
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            try{if(conn != null) conn.close();} catch(Exception e){e.printStackTrace();}
-            try{if(pstmt != null) pstmt.close();} catch(Exception e){e.printStackTrace();}
-            try{if(rs != null) rs.close();} catch(Exception e){e.printStackTrace();}
-        }
-        return false; // 회원가입 실패
-    }
+            while(rs.next()) {
+                return rs.getBoolean(1); // 이메일 주소 반환
+                 		}
+        } catch (SQLException e) {
+            e.printStackTrace();		}
+        return false; //데이터베이스 오류
+         }
 
     public boolean setUserEmailChecked(String userID){
         String SQL = "UPDATE USER SET userEmailChecked = true WHERE userID = ?";
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try{
-            conn = DatabaseUtil.getConnection();
-            pstmt = conn.prepareStatement(SQL);
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
             pstmt.setString(1, userID);
             pstmt.executeUpdate();
-            return true;
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            try{if(conn != null) conn.close();} catch(Exception e){e.printStackTrace();}
-            try{if(pstmt != null) pstmt.close();} catch(Exception e){e.printStackTrace();}
-            try{if(rs != null) rs.close();} catch(Exception e){e.printStackTrace();}
+            return true; // 이메일 등록 설정 성공
+
+         		}catch (SQLException e) {
+        e.printStackTrace();
         }
-        return false; // 회원가입 실패
-    }
+        return false; // 이메일 등록 설정 실패
+         	}
 }
